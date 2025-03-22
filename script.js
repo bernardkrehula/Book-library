21bd86ba91028005085091d54cbf3f242016f594
bernardkrehula <krehula.bernard456@gmail.com>
2025-03-22 16:27:50 +0100

    Push created books in array

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const newBookBtn = document.querySelector('.newBookBtn');
 const sendBtn = document.querySelector('.sendBtn');
 const main = document.querySelector('.main');
+const booksDiv = document.querySelector('.books');
 
-function bookCreator() {
-    let id;
+function bookCreator(title, author, pages) {
+    let id = crypto.randomUUID();
+    let bookTitle = title;
+    let bookAuthor = author;
+    let bookPages = pages;
+    const getId = () => { return id };
+    const getBookTitle = () => { return bookTitle };
+    const getBookAuthor = () => { return bookAuthor };
+    const getBookPages = () => { return bookPages };
+    return { getId, getBookTitle, getBookAuthor, getBookPages };
 }
 
 function manageBooks() {
@@ -12,8 +21,13 @@ function manageBooks() {
     const pushBooksInArray = (book) => {
         books.push(book);
     }
-    return { pushBooksInArray }
+    const returnArray = () => { return books };
+
+    const showBooksOnScreen = () => { books.map(book => displayNewBook(book))};
+
+    return { pushBooksInArray, returnArray, showBooksOnScreen };
 }
+const manager = manageBooks();
 
 function bookInfoDivCreator() {
     let html = `
@@ -21,15 +35,15 @@ function bookInfoDivCreator() {
             <button class="closeBookInfoBtn">X</button>
             <li>
                 <h4>Title of the book:</h4>
-                <input placeholder="Harry Potter">
+                <input placeholder="Harry Potter" class="bookTitle">
             </li>
             <li>
                 <h4>Author:</h4>
-                <input placeholder="J.K. Rowling">
+                <input placeholder="J.K. Rowling" class="author"> 
             </li>
             <li>
                 <h4>Number of pages:</h4>
-                <input placeholder="500">
+                <input type="number" placeholder="500" class="pages">
             </li>
             
             <div class="checkBoxDiv">
@@ -41,16 +55,40 @@ function bookInfoDivCreator() {
     main.insertAdjacentHTML('beforeend', html);   
 }
 
+function displayNewBook(book){
+    let html = `
+    <div class="addedBook">
+            <h3>${book.getBookTitle()}</h3>
+            <h4>${book.getBookAuthor()}</h4>
+            <p>${book.getBookPages()}</p>
+            <div class="addedBookButtons">
+                <button>Unread</button>
+                <button>Delete Book</button>
+            </div>
+        </div>
+    `
+    booksDiv.insertAdjacentHTML('beforeend', html)
+}
 newBookBtn.addEventListener('click', () => {
     bookInfoDivCreator();
 })
 
 main.addEventListener('click', (e) => {
     let bookInfoBtn = e.target.closest('button');
-    let closestDiv = e.target.closest('div');
+    let bookInfo = document.querySelector('.bookInfo');
+    
+    if(bookInfoBtn && bookInfoBtn.classList.contains('sendBtn')){
+        let bookTitle = document.querySelector('.bookTitle').value;
+        let author = document.querySelector('.author').value;
+        let bookPages = document.querySelector('.pages').value;
 
+        bookCreator(bookTitle, author, bookPages);
+        const newBook = bookCreator();
+        manager.pushBooksInArray(newBook);
+        main.removeChild(bookInfo);
+    }
     if(bookInfoBtn && bookInfoBtn.classList.contains('closeBookInfoBtn')){
-        main.removeChild(closestDiv);
+        main.removeChild(bookInfo);
     } 
 })
 /* bookInfo.addEventListener('click', (event) => {

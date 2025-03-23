76dcf66b1113e0cf3dba4a176482411ab0d3c829
bernardkrehula <krehula.bernard456@gmail.com>
2025-03-23 14:35:53 +0100

    Change isBookRead on click

--- script.js
+++ script.js
@@ -4,18 +4,21 @@ const bookForm = document.querySelector('.bookForm')
 const newBookBtn = document.querySelector('.newBookBtn');
 const closeBookFormBtn = document.querySelector('.closeBookFormBtn');
 const sendBtn = document.querySelector('.sendBtn');
+const checkBoxField = document.querySelector('.checkBox');
 
 
-function bookCreator(title, author, pages) {
+function bookCreator(title, author, pages, isChecked) {
     let id = crypto.randomUUID();
     let bookTitle = title;
     let bookAuthor = author;
     let bookPages = pages;
+    let checkBox = isChecked;
     const getId = () => { return id };
     const getBookTitle = () => { return bookTitle };
     const getBookAuthor = () => { return bookAuthor };
     const getBookPages = () => { return bookPages };
-    return { getId, getBookTitle, getBookAuthor, getBookPages };
+    const isBookRead = () => { return checkBox }
+    return { getId, getBookTitle, getBookAuthor, getBookPages, isBookRead };
 }
 //Uzet u obzir je li knjiga procitana ili nije, na unreadBtn dodati X ili kvacicu 
 //Dodat edit dugme, na click se svi podaci pretvaraju u input
@@ -32,9 +35,17 @@ function manageBooks() {
         books = books.filter(book => book.getId() != bookId);
         return books;
     }
+    const isBookRead = () => {
+        if(checkBoxField.checked == true){
+            return 'Read';
+        }
+        if(checkBoxField.checked == false) {
+            return 'Unread';
+        }
+    }
     const returnArray = () => { return books };
 
-    return { pushBooksInArray, returnArray, removeBookFromArray };
+    return { pushBooksInArray, returnArray, removeBookFromArray, isBookRead };
 }
 const manager = manageBooks();
 
@@ -45,17 +56,19 @@ function displayNewBook(book){
             <h4>${book.getBookAuthor()}</h4>
             <p>${book.getBookPages()}</p>
             <div class="addedBookButtons">
-                <button>Unread</button>
+                <button class="isRead">${book.isBookRead()}</button>
                 <button class="delete">Delete Book</button>
-                <button>Edit</button>
+                <button class="editBtn">Edit</button>
             </div>
         </div>
     `
     booksDiv.insertAdjacentHTML('beforeend', html)
 }
-newBookBtn.addEventListener('click', (e) => {
+
+newBookBtn.addEventListener('click', () => {
     bookForm.style.display = 'block';
 })
+
 bookForm.addEventListener('submit', (e) => {
     e.preventDefault();
     bookForm.style.display = 'none';
@@ -63,11 +76,16 @@ bookForm.addEventListener('submit', (e) => {
     let bookTitle = document.querySelector('.bookTitle').value;
     let author = document.querySelector('.author').value;
     let bookPages = document.querySelector('.pages').value;
-
-    const newBook = bookCreator(bookTitle, author, bookPages);
+    
+    const newBook = bookCreator(bookTitle, author, bookPages, manager.isBookRead());
     manager.pushBooksInArray(newBook);
     displayNewBook(newBook); 
 
+    bookTitle = document.querySelector('.bookTitle').value = '';
+    author = document.querySelector('.author').value = '';
+    bookPages = document.querySelector('.pages').value = '';
+    
+    checkBoxField.checked = false;
 })
 
 closeBookFormBtn.addEventListener('click', () => {
@@ -75,12 +93,24 @@ closeBookFormBtn.addEventListener('click', () => {
 }) 
 
 main.addEventListener('click', (e) => {
-    let deleteBtn = e.target.closest('button');
+    let bookBtns = e.target.closest('button');
     let bookDiv = e.target.closest('.addedBook');
- 
-    if(deleteBtn){
-        if(deleteBtn.className === 'delete'){
+   
+    if(bookBtns){
+        if(bookBtns.className === 'delete'){
             manager.removeBookFromArray(bookDiv.id);
+            booksDiv.removeChild(bookDiv);
+        }
+        if(bookBtns.className === 'editBtn'){
+
+        }
+        if(bookBtns.className === 'isRead'){
+            if(bookBtns.textContent === 'Read'){
+                bookBtns.innerText = 'Unread';
+            }
+            else {
+                bookBtns.innerText = 'Read';
+            }
         }
     }
 })
